aa78578d1e97b11bf39a1fb443cff98a43d9903b
kaiyuran <73236245+kaiyuran@users.noreply.github.com>
2023-11-25 20:55:22 -0500

    made text look good and fixed some bugs+added example data

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 Merger: a multitasker tool to be more productive!
+
+To run the program, run commandLine.py after getting all the requirements
+You do not need to use the default files to save, but remember to include the ABSOLUTE path
--- commandLine.py
+++ commandLine.py
@@ -1,12 +1,15 @@
 #Command line implementation
 import random
 import csv
+import colorama
+from colorama import Fore, Back, Style
 
+colorama.init(autoreset=True)
 
 def printList(list):
-    print("\nYour activity list:")
+    print(Back.MAGENTA+"\nYour activity list:")
     for i in range(len(list)):
-        print(str(i+1)+".", str(list[i][0]) + ":", list[i][1], "minutes")
+        print(Back.MAGENTA+str(i+1)+". " + str(list[i][0]) + ": " + str(list[i][1]) + " minutes")
 
 def readCSV(filename):
     with open(filename, 'r', newline='') as file:
@@ -21,14 +24,10 @@ def writeCSV(filename, data):
         for row in data:
             lists.writerow(row)
 
-        
-
-
 CSVPath = "./data.csv"
 txtPath = "./myActivityList.txt"
 
-
-print("""WELCOME TO MERGER""")
+print(Fore.GREEN +Style.BRIGHT+ "WELCOME TO MERGER")
 
 #Type of input
 correctInput = False
@@ -43,35 +42,33 @@ if typeOfInput == "1":
     #new list manually
     activityList = []
     newactivity = ""
-    print("Type out the different activities you have in a day in order, and the time it takes to complete in minutes wiht a space. To finish, type DONE")
+    print(Fore.RED+"\n\nType out the different activities you have in a day in order(use '_' instead of ' '), and the time it takes to complete in minutes with a space. To finish, type DONE")
     while newactivity.lower() != "done":
-        newactivity = input("Enter activity + time: ")
+        newactivity = input("\nEnter activity + time: ")
         if newactivity.lower() != "done":
             activityList.append(newactivity.split(" "))
             try:
                 activityList[-1][1] = int(activityList[-1][1])
             except:
-                activityList[-1][1] = float(activityList[-1][1])
+                try:
+                    activityList[-1][1] = float(activityList[-1][1])
+                except:
+                    pass
 else:
-    CSVPathOption = input("do you want to use a nonstandard path for your csv?(y/n): ")
+    CSVPathOption = input(Fore.YELLOW +Style.BRIGHT+ "\nDo you want to use a nonstandard path for your csv?(y/n): ")
     if CSVPathOption.lower() == "y":
         CSVPath = input("\nType the absolute path here: ")
     activityList = readCSV(CSVPath)
     
-
-print(activityList)
-
 #make sure no activity is a length of 1
 for row in activityList:
     if len(row) != 2:
-        row.pop()
-
-
+        activityList.remove(row)
 
 #Merging activities
 stillMerging = True
 totalSavedTime = 0
-print("Here are the activities to be merged and save time")
+print(Fore.LIGHTBLUE_EX + "\nHere are the activities to be merged and save time")
 while stillMerging:
     if len(activityList) > 1:
         activity1 = random.randint(0, len(activityList)-2)
@@ -79,7 +76,7 @@ while stillMerging:
         print("\nActivity 1:",activityList[activity1][0])
         print("Activity 2:",activityList[activity2][0])
         print("You could save", min(activityList[activity1][1],activityList[activity2][1]), "minutes!")
-        mergeDecision = input("Do you want to merge activities?(y/n): ")
+        mergeDecision = input(Fore.YELLOW+"Do you want to merge activities?(y/n): ")
         if mergeDecision.lower() == "y":
             totalSavedTime += min(activityList[activity1][1],activityList[activity2][1])
             # activity
@@ -102,31 +99,26 @@ while stillMerging:
         stillMerging = False
 print("In total, you have saved", totalSavedTime, "minute(s)!")
 
-
-
-
-
 #save as text file
-saveToTxtFile = input("Do you want to save your activity list as a text file? (y/n):")
+saveToTxtFile = input(Fore.YELLOW +Style.BRIGHT+ "\nDo you want to save your activity list as a text file? (y/n):")
 if saveToTxtFile.lower() == "y":
-    txtPathOption = input("Do you want to use a nonstandard path for your text file?(y/n): ")
+    txtPathOption = input(Fore.YELLOW +Style.BRIGHT+ "Do you want to use a nonstandard path for your text file?(y/n): ")
     if txtPathOption.lower() == "y":
         txtPath = input("\nType the absolute path here: ")
     with open(txtPath, "w") as textFile:
         textFile.write("My activity list:")
         for activityNum in range(len(activityList)):
             textFile.write("\n"+(str(activityNum+1)+". "+ str(activityList[activityNum][0]) + ": "+ str(activityList[activityNum][1])+ " minutes"))
-        print("Successfully saved Activity list!")
+        print(Fore.GREEN+Style.NORMAL+"Successfully saved Activity list!")
 
 #save as csv file
-saveToCSVFile = input("\nDo you want to save your activity list data as a csv file? (y/n):")
+saveToCSVFile = input(Fore.YELLOW +Style.BRIGHT+ "\nDo you want to save your activity list data as a csv file? (y/n):")
 if saveToCSVFile.lower() == "y":
-    CSVPathOption = input("Do you want to use a nonstandard path for your text file?(y/n): ")
+    CSVPathOption = input(Fore.YELLOW +Style.BRIGHT+ "Do you want to use a nonstandard path for your text file?(y/n): ")
     if CSVPathOption.lower() == "y":
         CSVPath = input("\nType the absolute path here: ")
     writeCSV(CSVPath, activityList)
-    print("Successfully saved Activity data!")
-print("\nGoodbye!")
+    print(Fore.GREEN+Style.NORMAL+"Successfully saved Activity data!")
     
+print(Fore.GREEN+Style.BRIGHT+"\nGoodbye!")
     
-
